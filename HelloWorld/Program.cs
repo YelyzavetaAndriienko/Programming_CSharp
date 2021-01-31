26d1f1d02f8eae7bdf55cf91c805595709323bb4
Andriienko Yelyzaveta <yelyzaveta.andriienko@ukma.edu.ua>
2021-01-31 10:09:17 +0200

    program now works with parameters

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Liza!");
+            Console.WriteLine("Hello " + args[0] + "!");
         }
     }
 }
